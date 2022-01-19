4c13ebd1be4df18f429f6099722dd7ca9cde90d4
Amarnath0510 <amarmuthu007@gmail.com>
2022-01-19 18:24:00 +0530

    commited and finished

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use(express.json());
-dotenv.config()
+
 const PORT = process.env.PORT;
 
 
